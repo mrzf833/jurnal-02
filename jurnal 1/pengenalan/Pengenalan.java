94b1d119f7ae656b3d977d93d86c82dae28ae615
mrzf833 <palaktais@gmail.com>
2021-09-28 17:07:50 +0700

    add jurnal 01

---
+++ jurnal 1/pengenalan/Pengenalan.java
@@ -0,0 +1,8 @@
+public class Pengenalan
+{
+	public static void main(String[] args){
+		System.out.println("Nama: Zulfa Iniko Falaq");
+		System.out.println("Nim: D3IF6706213090");
+		System.out.println("Kelas: 45-RPLA01");
+	}
+}
